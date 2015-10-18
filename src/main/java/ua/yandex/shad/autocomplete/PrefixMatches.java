1a28fb781d9b9a15dbe2dbc48722e62345fb6d08
Ruslan Sakevych <xlionell@gmail.com>
2015-10-18 13:46:24 +0300

    Clear code from useless comments.

--- src/main/java/ua/yandex/shad/autocomplete/PrefixMatches.java
+++ src/main/java/ua/yandex/shad/autocomplete/PrefixMatches.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ua.yandex.shad.autocomplete;
 
 import ua.yandex.shad.tries.Tuple;
 import ua.yandex.shad.tries.Trie;
 
-/**
- *
- * @author andrii
- */
 public class PrefixMatches {
 
     private Trie trie;
