9d072f87c005fd2de0cc3ddd043c1a604d914306
Ruslan Sakevych <xlionell@gmail.com>
2015-10-18 16:35:14 +0300

    Add tests for Load method. Now they are RED.

--- src/test/java/ua/yandex/shad/autocomplete/PrefixMatchesTest.java
+++ src/test/java/ua/yandex/shad/autocomplete/PrefixMatchesTest.java
@@ -3,6 +3,7 @@ package ua.yandex.shad.autocomplete;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,7 @@ import org.mockito.Mock;
 
 import org.mockito.runners.MockitoJUnitRunner;
 import ua.yandex.shad.tries.Trie;
+import ua.yandex.shad.tries.Tuple;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PrefixMatchesTest {
@@ -18,4 +20,44 @@ public class PrefixMatchesTest {
 
     @InjectMocks
     private PrefixMatches prefixMatches = new PrefixMatches();
+
+    private static Tuple first = new Tuple("one", 1);
+    private static Tuple second = new Tuple("two", 2);
+
+    @Test
+    public void testLoad_withNoParameters_sizeIsZero() {
+        int expectedSize = 0;
+
+        int actualSize = prefixMatches.load();
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void testLoad_withEmptyString_sizeIsZero() {
+        int expectedSize = 0;
+
+        int actualSize = prefixMatches.load("");
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void testLoad_withOneWord_sizeIsOne() {
+        int expectedSize = 1;
+
+        int actualSize = prefixMatches.load("one");
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void testLoad_withOneWord_containsThisWord() {
+        String strings = "one";
+        String expectedWord = "one";
+
+        prefixMatches.load(strings);
+
+        assertTrue(prefixMatches.contains(expectedWord));
+    }
 }
