74220934c2f4ba3beef293741fb3401e4ab733ff
Ruslan Sakevych <xlionell@gmail.com>
2015-10-19 13:41:55 +0300

    Modify test for PrefixMatches load method. Now there are RED tests.

--- src/test/java/ua/yandex/shad/autocomplete/PrefixMatchesTest.java
+++ src/test/java/ua/yandex/shad/autocomplete/PrefixMatchesTest.java
@@ -25,38 +25,62 @@ public class PrefixMatchesTest {
     private Tuple second = new Tuple("apple", 5);
 
     @Test
-    public void testLoad_noParameters_sizeIsZero() {
-        int expectedSize = 0;
+    public void testLoad_noParameters_nothingAdded() {
+        prefixMatches.load();
 
-        int actualSize = prefixMatches.load();
-
-        assertEquals(expectedSize, actualSize);
+        verify(trie, never()).add(any(Tuple.class));
     }
 
     @Test
-    public void testLoad_emptyString_sizeIsZero() {
-        int expectedSize = 0;
+    public void testLoad_emptyString_nothingAdded() {
         String strings = "";
 
-        int actualSize = prefixMatches.load(strings);
+        prefixMatches.load(strings);
 
-        assertEquals(expectedSize, actualSize);
+        verify(trie, never()).add(any(Tuple.class));
     }
 
     @Test
-    public void testLoad_oneWord_wordAdded() {
+    public void testLoad_stringWithOneWord_wordAdded() {
         String strings = "one";
 
         prefixMatches.load(strings);
+
         verify(trie, times(1)).add(Matchers.eq(first));
         verify(trie).add(Matchers.eq(first));
     }
 
     @Test
-    public void testLoad_twoWordsSeparatedWithSpace_addedTwoWords() {
+    public void testLoad_stringWithTwoWordsSeparatedWithSpace_addedTwoWords() {
         String strings = "one apple";
 
         prefixMatches.load(strings);
+
+        verify(trie, times(1)).add(Matchers.eq(first));
+        verify(trie, times(1)).add(Matchers.eq(second));
+        verify(trie, times(2)).add(any(Tuple.class));
+        verify(trie).add(Matchers.eq(first));
+        verify(trie).add(Matchers.eq(second));
+    }
+
+    @Test
+    public void testLoad_stringWithTwoWordsSeparatedWithTab_addedTwoWords() {
+        String strings = "one   apple";
+
+        prefixMatches.load(strings);
+
+        verify(trie, times(1)).add(Matchers.eq(first));
+        verify(trie, times(1)).add(Matchers.eq(second));
+        verify(trie, times(2)).add(any(Tuple.class));
+        verify(trie).add(Matchers.eq(first));
+    }
+
+    @Test
+    public void testLoad_twoStringsWithOneWordEach_addedTwoWords() {
+        String[] strings = {"one", "apple"};
+
+        prefixMatches.load(strings);
+
         verify(trie, times(1)).add(Matchers.eq(first));
         verify(trie, times(1)).add(Matchers.eq(second));
         verify(trie, times(2)).add(any(Tuple.class));
