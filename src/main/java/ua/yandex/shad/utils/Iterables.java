191d3e9f48885659d85725e03bc2ee9568a6ad08
Ruslan Sakevych <xlionell@gmail.com>
2015-10-23 20:15:17 +0300

    Document Iterables class.

--- src/main/java/ua/yandex/shad/utils/Iterables.java
+++ src/main/java/ua/yandex/shad/utils/Iterables.java
@@ -1,7 +1,36 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Ruslan Sakevych
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package ua.yandex.shad.utils;
 
 import java.util.Iterator;
 
+/**
+ * Class is used to compare two instances of classes that implements Iterable
+ * interface.
+ * @param <T> type of objects in Iterable
+ */
 public class Iterables<T> {
     public String toString(Iterable<T> iterable) {
         Iterator<T> iterator = iterable.iterator();
